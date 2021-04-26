a42054242f126251f55ebbb7c9c5487c3e02e3df
igalv <vilenskyigal@gmail.com>
2021-04-26 14:42:09 +0200

    styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,14 @@ let inputField = document.querySelector(`#inputField`);
 
 addToDoBtn.addEventListener(`click`, function () {
   let paragraph = document.createElement("p");
+  paragraph.classList.add(`parStyle`);
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener(`click`, function () {
+    paragraph.style.textDecoration = `line-through`;
+  });
+  paragraph.addEventListener(`dblclick`, function () {
+    toDoContainer.removeChild(paragraph);
+  });
 });
