7b660753185dda54e489ce0550243830c00f9e87
igalv <vilenskyigal@gmail.com>
2021-04-26 13:02:31 +0200

    js

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+let addToDoBtn = document.querySelector(`#addToDo`);
+let toDoContainer = document.querySelector(`#toDoContainer`);
+let inputField = document.querySelector(`#inputField`);
+
+addToDoBtn.addEventListener(`click`, function () {
+  let paragraph = document.createElement("p");
+});
